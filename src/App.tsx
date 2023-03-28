9c553b34df64621f3fdffb8a8a6e50c1c110cd69
Muhammed Kerem Kuzu <kerem_kuzu@gmx.de>
2023-03-28 03:20:16 +0200

    feat: add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,25 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { SafeAreaView, StyleSheet, useColorScheme } from 'react-native';
+import { useColorScheme } from 'react-native';
 import Home from './screens/Home';
-import { Colors } from './styles/Globals';
-
-const styles = StyleSheet.create({
-  viewStyles: {
-    width: '100%',
-    height: '100%',
-  },
-});
-
-const Tab = createBottomTabNavigator();
-
-function Navigation(): JSX.Element {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-    </Tab.Navigator>
-  );
-}
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode
-      ? Colors.backgroundDark
-      : Colors.backgroundLight,
-  };
+  const Tab = createBottomTabNavigator();
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <>
       <NavigationContainer>
-        <Navigation />
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+        </Tab.Navigator>
       </NavigationContainer>
-    </SafeAreaView>
+    </>
   );
 }
 
