afe5efb4021ca70782bf337493cd50fa7d0ac312
Muhammed Kerem Kuzu <kerem_kuzu@gmx.de>
2023-03-28 07:04:10 +0200

    feat: add react-native-vector-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { useColorScheme } from 'react-native';
+import { SafeAreaView, StyleSheet, useColorScheme, View } from 'react-native';
 import Home from './screens/Home';
+import { Colors } from './styles/Globals';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   const Tab = createBottomTabNavigator();
 
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const styles = StyleSheet.create({
+    safeArea: {
+      flex: 1,
+    },
+    bg: {
+      backgroundColor: isDarkMode
+        ? Colors.backgroundDark
+        : Colors.backgroundLight,
+      flex: 1,
+    },
+  });
+
   return (
-    <>
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen
-            name="Home"
-            component={Home}
-            options={{ headerShown: false }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </>
+    <View style={styles.bg}>
+      <SafeAreaView style={styles.safeArea}>
+        <NavigationContainer>
+          <Tab.Navigator screenOptions={{ headerShown: false }}>
+            <Tab.Screen name="Home" component={Home} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </View>
   );
 }
 
